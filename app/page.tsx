07468d7ac494f55e6a67374168ba77643df4a296
rinne <nyannnyannnyan282@gmail.com>
2026-05-31 19:57:30 +0900

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,18 @@
 import { supabase } from '@/lib/supabaseClient'
 
-export default async function CharacterPage({
-  params,
-}: {
-  params: Promise<{ id: string }>
-}) {
-  const { id } = await params
-
+export default async function Page() {
   const { data, error } = await supabase
     .from('characters')
     .select('*')
-    .eq('id', id)
-    .single()
+    .limit(10)
 
   if (error) {
     return <div>エラー: {error.message}</div>
   }
 
-  if (!data) {
-    return <div>データがない</div>
-  }
-
   return (
     <div>
-      <h1>[characters/[id]/page.tsx]</h1>
-
-      <h2>{data.name}</h2>
       <pre>{JSON.stringify(data, null, 2)}</pre>
-
-      <div>aa: {id}</div>
     </div>
   )
 }
